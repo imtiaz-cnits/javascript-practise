0299892d0e38c1add49e818bf05c18036e32a7cc
Imtiaz Ahmed <ia64744@gmail.com>
2023-07-18 13:39:22 +0600

    do...while loop

--- javascritp-loop/script.js
+++ javascritp-loop/script.js
@@ -11,3 +11,12 @@ while(index2 <=3) {
     console.log("Hi Rahat", index2);
     index2++;
 }
+
+
+//do...while loop
+let service = 10;
+do {
+    console.log('Hi Robi', service);
+    service++;
+} while(service <= 15);
+console.log(service);

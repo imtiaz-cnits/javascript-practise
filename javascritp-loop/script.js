ee2cdceeeea084c0af5b6b5270468594142f6529
Imtiaz Ahmed <ia64744@gmail.com>
2023-07-19 15:04:56 +0600

    js-loop

--- javascritp-loop/script.js
+++ javascritp-loop/script.js
@@ -20,3 +20,37 @@ do {
     service++;
 } while(service <= 15);
 console.log(service);
+
+
+//for...in loop
+const man = {
+    name : 'Imtiaz',
+    age : 24
+}
+
+for (let key in man) {
+    console.log(key, man[key]);
+}
+
+let arr = [1,2,3,4,5];
+console.log(arr);
+
+for (let indx in arr) {
+    console.log(indx, arr[indx]);
+}
+
+
+//for...of loop
+let power = [6,7,8,9,0];
+
+for (let act of power) {
+    console.log(act);
+}
+
+
+//nested loop
+for (let imtiaz = 1; imtiaz <= 3; imtiaz++) {
+    for (let anika = 1; anika <= 3; anika++) {
+        console.log(imtiaz, anika);
+    }
+}

47451bcd5a0c7eb1fdbc919bd175e576b9c8eb6e
Imtiaz Ahmed <ia64744@gmail.com>
2023-07-23 18:03:53 +0600

    rest parameter

--- javascript-function/script.js
+++ javascript-function/script.js
@@ -26,3 +26,32 @@ function total(number1, number2 = 35, number3 = 45) {
 }
 
 console.log(total(12));
+
+// Function Expression
+const calcSumExp = function (numb1, numb2) {
+  let sum1 = numb1 + numb2;
+  return sum1;
+};
+
+console.log(calcSumExp(40, 70));
+
+// Arrow Function 01
+const calcSumArr = (numbe1, numbe2) => numbe1 + numbe2;
+console.log(calcSumArr(50, 80));
+
+// Arrow Function 02
+const mulSumArr = (point1) => point1 * 2;
+console.log(mulSumArr(8));
+
+// Rest Parameter
+function multiply(...args) {
+  console.log(args);
+
+  let mulp = 1;
+  for (let total of args) {
+    mulp = mulp * total;
+  }
+  return mulp;
+}
+
+console.log(multiply(1, 2, 3, 4, 5, 6, 8));

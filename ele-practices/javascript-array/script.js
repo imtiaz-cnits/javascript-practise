2301ae576cc62e70fa7dd42cc3c6aaa06d7fe8e0
Imtiaz Ahmed <ia64744@gmail.com>
2023-07-25 09:55:53 +0600

    array-sorting

--- ele-practices/javascript-array/script.js
+++ ele-practices/javascript-array/script.js
@@ -1,4 +1,39 @@
-let arr = [2, 3, 4, 5];
+// Iterating an Array
+const numbers = [1, 2, 3, 4, 5];
 
-console.log(arr);
-console.log(arr.length);
+//using for loop
+for (let index = 0; index < numbers.length; index++) {
+  console.log(index, numbers[index]);
+}
+
+// using for...of loop
+for (let num of numbers) {
+  console.log(num);
+}
+
+// using for...each loop
+numbers.forEach(function (num, index) {
+  console.log(index, num);
+});
+
+// Sort and reversing an array
+const srt = [6, 3, , 7, 2, 1, 9];
+
+srt.sort();
+console.log(srt);
+
+let developers = [
+  { name: "Imtiaz", age: 26 },
+  { name: "Rahat", age: 21 },
+  { name: "Anika", age: 24 },
+];
+
+developers
+  .sort(function (d1, d2) {
+    if (d1.age > d2.age) return +1;
+    if (d1.age === d2.age) return 0;
+    if (d1.age < d2.age) return -1;
+  })
+  .reverse();
+
+console.log(developers);

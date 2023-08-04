d6206801d8732e6f808e2e9b5bcf5f40539a2cf2
Imtiaz Ahmed <ia64744@gmail.com>
2023-08-04 19:00:31 +0600

    completed async, fetch and api usage

--- ele-practices/asynchronous-js-ajax,fetch-api/script.js
+++ ele-practices/asynchronous-js-ajax,fetch-api/script.js
@@ -21,11 +21,22 @@ console.log(user);
 
 console.log("After");
 
-// Callback Function
+// Callback & Callback Hell
 
 fetchUsers(1245, function (user) {
   console.log(user);
+  sendMail(user.email, function (response) {
+    console.log(response.success);
+  });
 });
+
+function sendMail(userMail, callback) {
+  setTimeout(function () {
+    console.log("Message is Sending...");
+    const response = { success: true };
+    callback(response);
+  }, 3000);
+}
 
 function fetchUsers(userId, callback) {
   setTimeout(function () {
@@ -38,3 +49,58 @@ function fetchUsers(userId, callback) {
     callback(fetchedUser);
   }, 2000);
 }
+
+//Promise
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // resolve(1);
+    reject(new Error("Have some error!"));
+  }, 8000);
+});
+
+console.log(promise);
+
+promise
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// Handeling Multiple Promises
+
+const promise1 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve(1), 2000);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error("Have an Issue")), 2000);
+});
+
+Promise.all([promise1, promise2])
+  .then((res1) => console.log(res1))
+  .catch((err1) => console.log(err1));
+
+// Fetch Web API
+
+const gitAPI = fetch("https://api.github.com/users/andrew");
+
+gitAPI
+  .then((resp) => resp.json())
+  .then((profile) => console.log(profile))
+  .catch((errors) => console.log(errors));
+
+console.log(gitAPI);
+
+// Fetch API using Async and Await
+
+async function getInfoFromGit() {
+  const data = await fetch("https://api.github.com/users/andrew");
+
+  const profiles = await data.json();
+  console.log(profiles);
+}
+
+getInfoFromGit();

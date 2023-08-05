e6a02db3ec4bc4750072e7bf936eaf18183d53d7
imtiazahmed80 <ia64744@gmail.com>
2023-08-05 20:48:59 +0600

    parsing data and stringify

--- ele-practices/Excise_File/Microblog/index.js
+++ ele-practices/Excise_File/Microblog/index.js
@@ -1,8 +1,19 @@
-localStorage.setItem("name", "Imtiaz");
-localStorage.setItem("age", 26);
+// localStorage.setItem("name", "Imtiaz");
+// localStorage.setItem("age", 26);
 
-const age = localStorage.getItem("age");
-console.log(age);
+// const age = localStorage.getItem("age");
+// console.log(age);
 
-localStorage.setItem("name", "Anika");
-console.log(localStorage.getItem("name"));
+// localStorage.setItem("name", "Anika");
+// console.log(localStorage.getItem("name"));
+
+// // // // Stringify and Parsing Data from Local Storage // // // //
+const users = [
+  { name: "Imtiaz", age: 26 },
+  { name: "Anika", age: 24 },
+];
+
+localStorage.setItem("users", JSON.stringify(users));
+const userFrom = localStorage.getItem("users");
+
+console.log(JSON.parse(userFrom));

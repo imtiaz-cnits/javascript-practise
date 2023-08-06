50e034afd5d9a7f0954574556be8b03c05f9e6fa
imtiazahmed80 <ia64744@gmail.com>
2023-08-06 20:41:09 +0600

    api fetch  practicing

--- ele-practices/asynchronous-js-ajax,fetch-api/script.js
+++ ele-practices/asynchronous-js-ajax,fetch-api/script.js
@@ -1,106 +1,123 @@
 // Asynchronous Programming
 
-console.log("Before");
+// console.log("Before");
 
-setTimeout(function () {
-  console.log("Message will load after 3 seconds");
-}, 2000);
+// setTimeout(function () {
+//   console.log("Message will load after 3 seconds");
+// }, 2000);
 
-function fetchUser() {
-  setTimeout(function () {
-    console.log("Message will load after 3 seconds");
-    return {
-      name: "Imtiaz",
-      age: 26,
-    };
-  }, 2000);
-}
+// function fetchUser() {
+//   setTimeout(function () {
+//     console.log("Message will load after 3 seconds");
+//     return {
+//       name: "Imtiaz",
+//       age: 26,
+//     };
+//   }, 2000);
+// }
 
-const user = fetchUser();
-console.log(user);
+// const user = fetchUser();
+// console.log(user);
 
-console.log("After");
+// console.log("After");
 
 // Callback & Callback Hell
 
-fetchUsers(1245, function (user) {
-  console.log(user);
-  sendMail(user.email, function (response) {
-    console.log(response.success);
-  });
-});
+// fetchUsers(1245, function (user) {
+//   console.log(user);
+//   sendMail(user.email, function (response) {
+//     console.log(response.success);
+//   });
+// });
 
-function sendMail(userMail, callback) {
-  setTimeout(function () {
-    console.log("Message is Sending...");
-    const response = { success: true };
-    callback(response);
-  }, 3000);
-}
+// function sendMail(userMail, callback) {
+//   setTimeout(function () {
+//     console.log("Message is Sending...");
+//     const response = { success: true };
+//     callback(response);
+//   }, 3000);
+// }
 
-function fetchUsers(userId, callback) {
-  setTimeout(function () {
-    console.log("Imtiaz is Web Developer");
-    const fetchedUser = {
-      id: userId,
-      name: "Imtiaz",
-      email: "imtiaz@gmail.com",
-    };
-    callback(fetchedUser);
-  }, 2000);
-}
+// function fetchUsers(userId, callback) {
+//   setTimeout(function () {
+//     console.log("Imtiaz is Web Developer");
+//     const fetchedUser = {
+//       id: userId,
+//       name: "Imtiaz",
+//       email: "imtiaz@gmail.com",
+//     };
+//     callback(fetchedUser);
+//   }, 2000);
+// }
 
 //Promise
 
-const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    // resolve(1);
-    reject(new Error("Have some error!"));
-  }, 8000);
-});
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     // resolve(1);
+//     reject(new Error("Have some error!"));
+//   }, 8000);
+// });
 
-console.log(promise);
+// console.log(promise);
 
-promise
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// promise
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
 // Handeling Multiple Promises
 
-const promise1 = new Promise((resolve, reject) => {
-  setTimeout(() => resolve(1), 2000);
-});
+// const promise1 = new Promise((resolve, reject) => {
+//   setTimeout(() => resolve(1), 2000);
+// });
 
-const promise2 = new Promise((resolve, reject) => {
-  setTimeout(() => reject(new Error("Have an Issue")), 2000);
-});
+// const promise2 = new Promise((resolve, reject) => {
+//   setTimeout(() => reject(new Error("Have an Issue")), 2000);
+// });
 
-Promise.all([promise1, promise2])
-  .then((res1) => console.log(res1))
-  .catch((err1) => console.log(err1));
+// Promise.all([promise1, promise2])
+//   .then((res1) => console.log(res1))
+//   .catch((err1) => console.log(err1));
 
 // Fetch Web API
 
-const gitAPI = fetch("https://api.github.com/users/andrew");
+// const gitAPI = fetch("https://api.github.com/users/andrew");
 
-gitAPI
-  .then((resp) => resp.json())
-  .then((profile) => console.log(profile))
-  .catch((errors) => console.log(errors));
+// gitAPI
+//   .then((resp) => resp.json())
+//   .then((profile) => console.log(profile))
+//   .catch((errors) => console.log(errors));
 
-console.log(gitAPI);
+// console.log(gitAPI);
 
 // Fetch API using Async and Await
 
-async function getInfoFromGit() {
-  const data = await fetch("https://api.github.com/users/andrew");
+const form = document.querySelector("#form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const input = document.querySelector("input").value;
 
-  const profiles = await data.json();
-  console.log(profiles);
-}
+  async function getInfoFromGit() {
+    const data = await fetch(`https://api.github.com/users/${input}`);
 
-getInfoFromGit();
+    const profiles = await data.json();
+    console.log(profiles);
+
+    const profImg = document.querySelector("#img");
+    profImg.src = profiles.avatar_url;
+    const p = document.createElement("p");
+    p.innerText = profiles.name;
+
+    const info = document.querySelector(".info");
+    info.append(p);
+
+    const repo = document.querySelector("#repo");
+    repo.innerText = profiles.repos_url;
+  }
+
+  getInfoFromGit();
+});

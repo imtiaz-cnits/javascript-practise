8bd92274cafae06e1d541f0fd809ca56719369ec
Imtiaz Ahmed <ia64744@gmail.com>
2023-08-10 00:14:53 +0600

    e-wallet running

--- ele-practices/Excise_File/Project 2/eWallet - Start/index.js
+++ ele-practices/Excise_File/Project 2/eWallet - Start/index.js
@@ -3,5 +3,42 @@
 document.querySelector("#ewallet-form").addEventListener("submit", (e) => {
   e.preventDefault();
 
-  console.log(e);
+  const type = document.querySelector(".add__type").value;
+  const desc = document.querySelector(".add__description").value;
+  const value = document.querySelector(".add__value").value;
+
+  console.log(type, desc, value);
+
+  const newHtml = `
+  <div class="item">
+  <div class="item-description-time">
+      <div class="item-description">
+          <p>${desc}</p>
+      </div>
+      <div class="item-time">
+          <p>25 Feb, 06:45 PM</p>
+      </div>
+  </div>
+  <div class="item-amount expense-amount">
+      <p>${type}$${value}</p>
+  </div>
+</div>`;
+
+  console.log(newHtml);
 });
+
+/*
+<div class="item">
+    <div class="item-description-time">
+        <div class="item-description">
+            <p>Buy a physics book</p>
+        </div>
+        <div class="item-time">
+            <p>25 Feb, 06:45 PM</p>
+        </div>
+    </div>
+    <div class="item-amount expense-amount">
+        <p>-$78</p>
+    </div>
+</div>
+*/

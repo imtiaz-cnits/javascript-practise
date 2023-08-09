09a661ec147ec47faabd1dddbf2c84cf4df990af
imtiazahmed80 <ia64744@gmail.com>
2023-08-09 16:23:36 +0600

    monster app done

--- ele-practices/Excise_File/Project 1/MonsterFinder - Start/index.js
+++ ele-practices/Excise_File/Project 1/MonsterFinder - Start/index.js
@@ -1,23 +1,26 @@
 // I wish you good luck and happy coding 🥰🤠🥳🥳💯💯
-import { monsters } from "./monsters.js00";
+import { monsters } from "./monsters.js";
 console.log(monsters);
 
-showMonster();
-function showMonster() {
+for (let monsterDiv of monsters) {
+  showMonster(monsterDiv);
+}
+
+function showMonster(monsterDiv) {
   const monster = document.createElement("div");
   monster.className = "monster";
 
   const img = document.createElement("img");
-  img.src = "https://robohash.org/8?set=set2";
+  img.src = `https://robohash.org/${monsterDiv.id}?set=set2`;
   img.alt = "Kazi Ariyan";
 
   const mname = document.createElement("p");
   mname.className = "name";
-  mname.innerText = "Kazi Ariyan";
+  mname.innerText = monsterDiv.name;
 
   const email = document.createElement("email");
   email.className = "email";
-  email.innerText = "info@easylearningbd.com";
+  email.innerText = monsterDiv.email;
 
   monster.append(img, mname, email);
   document.querySelector(".monsters").append(monster);
@@ -47,3 +50,30 @@ function showError() {
           <h1>🧟‍♂️ No Monster Found 🧟‍♂️</h1>
         </div>
 */
+
+// Monster Searching System
+document.querySelector("#search-monster").addEventListener("keyup", (e) => {
+  const keyword = e.target.value.toLowerCase();
+
+  const findMonster = document.querySelectorAll(".monster");
+
+  let notFound = true;
+
+  for (let monster of findMonster) {
+    const name = monster.children[1].innerText.toLowerCase();
+    const email = monster.children[2].innerText.toLowerCase();
+
+    if (name.includes(keyword) || email.includes(keyword)) {
+      monster.style.display = "block";
+      notFound = false;
+    } else {
+      monster.style.display = "none";
+    }
+  }
+
+  if (notFound) {
+    document.querySelector(".not-found").style.display = "block";
+  } else {
+    document.querySelector(".not-found").style.display = "none";
+  }
+});

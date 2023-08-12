72ccfdd885104ac5d7df0fb727769f6f1e0dc91a
Imtiaz Ahmed <ia64744@gmail.com>
2023-08-13 00:09:59 +0600

    e-wallet app project done

--- ele-practices/Excise_File/Project 2/eWallet - Start/index.js
+++ ele-practices/Excise_File/Project 2/eWallet - Start/index.js
@@ -25,6 +25,35 @@ document.querySelector("#ewallet-form").addEventListener("submit", (e) => {
   }
 });
 
+showItems();
+
+function showItems() {
+  let items = getItemfromLS();
+
+  const collection = document.querySelector(".collection");
+
+  for (let item of items) {
+    const newHtml = `
+  <div class="item">
+  <div class="item-description-time">
+      <div class="item-description">
+          <p>${item.desc}</p>
+      </div>
+      <div class="item-time">
+          <p>${item.time}</p>
+      </div>
+  </div>
+  <div class="item-amount ${
+    item.type === "+" ? "income-amount" : "expense-amount"
+  }">
+      <p>${item.type}$${seprator(item.value)}</p>
+  </div>
+  </div>`;
+
+    collection.insertAdjacentHTML("afterbegin", newHtml);
+  }
+}
+
 function addItems(type, desc, value) {
   const time = getFormattedTime();
 
@@ -39,16 +68,92 @@ function addItems(type, desc, value) {
       </div>
   </div>
   <div class="item-amount ${type === "+" ? "income-amount" : "expense-amount"}">
-      <p>${type}$${value}</p>
+      <p>${type}$${seprator(value)}</p>
   </div>
 </div>`;
 
   const collection = document.querySelector(".collection");
   collection.insertAdjacentHTML("afterbegin", newHtml);
+
+  addItemstoLS(type, desc, value, time);
+  showTotalIncome();
+  showTotalExpense();
+  showTotalBalance();
 }
 
 function resetForm() {
   document.querySelector(".add__type").value = "+";
   document.querySelector(".add__description").value = "";
   document.querySelector(".add__value").value = "";
+}
+
+function getItemfromLS() {
+  let items = localStorage.getItem("items");
+  if (items) {
+    items = JSON.parse(items);
+  } else {
+    items = [];
+  }
+  return items;
+}
+
+function addItemstoLS(type, desc, value, time) {
+  let items = getItemfromLS();
+  items.push({ type, desc, value, time });
+  localStorage.setItem("items", JSON.stringify(items));
+}
+
+showTotalIncome();
+
+function showTotalIncome() {
+  let items = getItemfromLS();
+  let totalIncome = 0;
+  for (let item of items) {
+    if (item.type === "+") {
+      totalIncome += parseInt(item.value);
+    }
+  }
+  console.log(totalIncome);
+  document.querySelector(".income__amount p").innerText = `$${seprator(
+    totalIncome
+  )}`;
+}
+
+showTotalExpense();
+
+function showTotalExpense() {
+  let items = getItemfromLS();
+  let totalExpense = 0;
+  for (let item of items) {
+    if (item.type === "-") {
+      totalExpense += parseInt(item.value);
+    }
+  }
+  console.log(totalExpense);
+  document.querySelector(".expense__amount p").innerText = `$${seprator(
+    totalExpense
+  )}`;
+}
+
+showTotalBalance();
+
+function showTotalBalance() {
+  let items = getItemfromLS();
+  let balance = 0;
+
+  for (let item of items) {
+    if (item.type === "+") {
+      balance += parseInt(item.value);
+    } else {
+      balance -= parseInt(item.value);
+    }
+  }
+  document.querySelector(".balance__amount p").innerText = seprator(balance);
+
+  document.querySelector("header").className = balance >= 0 ? "green" : "red";
+}
+
+function seprator(amount) {
+  amount = parseInt(amount);
+  return amount.toLocaleString();
 }

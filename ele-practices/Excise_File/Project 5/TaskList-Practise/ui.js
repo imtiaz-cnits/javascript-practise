e3beef951fc66c1600202bc91acc4b7b149679a4
imtiazahmed80 <ia64744@gmail.com>
2023-08-20 15:37:33 +0600

    tasklist project completed

--- ele-practices/Excise_File/Project 5/TaskList-Practise/ui.js
+++ ele-practices/Excise_File/Project 5/TaskList-Practise/ui.js
@@ -85,7 +85,7 @@ UI.prototype.updateTask = function (e) {
   const tasks = document.querySelectorAll(".task-title");
 
   if (taskTitle.length > 0) {
-    ls.updateTasks(taskId, taskTitle);
+    ls.updateTask(taskId, taskTitle);
 
     tasks.forEach((title) => {
       if (title.parentElement.parentElement.dataset.createdat === taskId) {
@@ -94,6 +94,15 @@ UI.prototype.updateTask = function (e) {
     });
   }
 
+  document.querySelector("#newtaskID").value = "";
+  document.querySelector("#updateTaskId").value = "";
+
+  document.querySelector(".AddTaskBtn").style.display = "inline";
+  document.querySelector(".EditTaskBtn").style.display = "none";
+  document.querySelector(".CancelTaskBtn").style.display = "none";
+};
+
+UI.prototype.cancelTask = function (e) {
   document.querySelector("#newtaskID").value = "";
   document.querySelector("#updateTaskId").value = "";
 

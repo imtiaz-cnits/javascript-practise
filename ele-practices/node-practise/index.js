7dd8dc61ad4784f0670017a78d4becaf8beae1a5
Imtiaz Ahmed <ia64744@gmail.com>
2023-08-23 00:21:22 +0600

    node js calculator project

--- ele-practices/node-practise/index.js
+++ ele-practices/node-practise/index.js
@@ -1,9 +1,27 @@
-// Understanding Modules
+const add = require("./calculator/add");
+const sub = require("./calculator/sub");
+const mul = require("./calculator/mul");
+const div = require("./calculator/div");
 
-const { add, sub } = require("./add");
+// console.log(add(5, 6));
+// console.log(sub(13, 6));
+// console.log(mul(5, 6));
+// console.log(div(12, 6));
 
-console.log(add(5, 3));
-console.log(sub(12, 5));
+const a = parseInt(process.argv[2]);
+const oprt = process.argv[3];
+const b = parseInt(process.argv[4]);
 
-console.log(__dirname);
-console.log(__filename);
+console.log(a, oprt, b);
+
+if (oprt === "+") {
+  console.log(add(a, b));
+} else if (oprt === "-") {
+  console.log(sub(a, b));
+} else if (oprt === "*") {
+  console.log(mul(a, b));
+} else if (oprt === "/") {
+  console.log(div(a, b));
+} else {
+  console.log("Invalid");
+}

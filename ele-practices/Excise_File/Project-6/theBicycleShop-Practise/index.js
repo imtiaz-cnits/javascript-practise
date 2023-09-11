5df0e5c63192e7da1394146ced98f8be2171ddeb
imtiazahmed80 <ia64744@gmail.com>
2023-09-11 17:19:45 +0600

    parsing url start

--- ele-practices/Excise_File/Project-6/theBicycleShop-Practise/index.js
+++ ele-practices/Excise_File/Project-6/theBicycleShop-Practise/index.js
@@ -1,6 +1,9 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+  if (req.url === "/favicon.ico") return;
+  console.log(req.url);
+
   res.writeHead(200, { "Content-Type": "text/html" });
   res.end("<h1>Welcome to the Bicycle Shop</h1>");
 });

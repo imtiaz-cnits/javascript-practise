463ef95b7a6c7569ea29f5ebc9337e305e6fd674
Imtiaz Ahmed <ia64744@gmail.com>
2023-09-17 02:12:03 +0600

    declaring routes

--- ele-practices/Excise_File/Project-6/theBicycleShop-Practise/index.js
+++ ele-practices/Excise_File/Project-6/theBicycleShop-Practise/index.js
@@ -1,8 +1,17 @@
 const http = require("http");
+const url = require("url");
 
 const server = http.createServer((req, res) => {
   if (req.url === "/favicon.ico") return;
   console.log(req.url);
+
+  console.log(req.headers);
+
+  const myUrl = new URL(req.url, "http://`${req.headers.host}`/");
+  const pathName = myUrl.pathname;
+  const id = myUrl.searchParams.get("id");
+
+  console.log(pathName, id);
 
   res.writeHead(200, { "Content-Type": "text/html" });
   res.end("<h1>Welcome to the Bicycle Shop</h1>");

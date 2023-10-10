89613841ce8476b877d793c088c35790e6d3a6a2
imtiazahmed80 <ia64744@gmail.com>
2023-10-11 02:33:28 +0600

    ejs basic complete

--- ele-practices/express-js/index.js
+++ ele-practices/express-js/index.js
@@ -2,8 +2,26 @@
 const express = require('express');
 const app = express();
 
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(express.json())
+
+
+// Introducing Middleware
+app.use((req, res, next) => {
+  console.log(req.method);
+  console.log(req.protocol);
+  console.log(req.get('host'));
+  console.log(req.originalUrl);
+
+  next();
+})
+
+
 //Creating route
-app.get('/', (req,res) => {
+app.get('/', (req,res, next) => {
   res.send('<h1>Hi Imtiaz Ahmed</h1>')
 });
 
@@ -14,7 +32,8 @@ app.get('/about', (req,res) => {
 });
 
 app.post('/login', (req,res) => {
-  console.log(req.body);
+  console.log(req.body.email);
+  console.log(req.body.password);
 
   res.send("User Login Successfully");
 })

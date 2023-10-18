c6bac27fd40cd54ef44e3803906aacdf7a4c8bd4
imtiazahmed80 <ia64744@gmail.com>
2023-10-18 08:09:30 +0600

    routing setup done

--- ele-projects/theBicycleShop/index.js
+++ ele-projects/theBicycleShop/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
 
+const bicycles = require('./data/data.json');
+
 app.get('/', (req, res) => {
-    res.send("Hello Imtiaz");
+    res.send(bicycles);
 });
+
+app.get('/bicycle', (req, res) => {
+    console.log(req.query.id);
+
+    const bicycle = bicycles.find(b => b.id === req.query.id);
+    res.send(bicycle);
+});
+
+
 
 
 app.listen('3000', () => console.log('Server is running at port 3000'));

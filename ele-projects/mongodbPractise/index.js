a7b641a543850063061cc0fb1b777071428a7ee0
Imtiaz Ahmed <ia64744@gmail.com>
2023-11-27 10:03:57 +0600

    mongodb running

--- ele-projects/mongodbPractise/index.js
+++ ele-projects/mongodbPractise/index.js
@@ -1,3 +1,46 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/test').then(() => 
 console.log('Database is Connected')).catch((err) => console.error(err));
+
+// Schema (Shape of a Document)
+const userSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    isMarried: Boolean,
+    salary: Number,
+    gender: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+// Problem Solving
+// Find those users whose age is greater then 40 or they are unmarried
+// Find only name
+// Sort them by name
+
+async function query(){
+    const users = await User.findById('6027d1fd8d4a543a2049d1a0');
+    users.isMarried = true;
+    users.save();
+
+    console.log(users);
+}
+
+query();
+
+
+// async function storeInformation(){
+//     const user = new User({
+//         name: 'Imtiaz Ahmed',
+//         age: 27,
+//         isMarried: true,
+//         salary: 40000,
+//         gender: 'Male'
+//     });
+//     await user.save();
+//     console.log(user);
+// }
+
+// storeInformation();
+
+

9d3cb495a7af528d1dfce4abb73f922ea8c14177
Imtiaz Ahmed <ia64744@gmail.com>
2023-12-23 22:36:32 +0600

    finished video 209

--- ele-projects/restApiMongoose/index.js
+++ ele-projects/restApiMongoose/index.js
@@ -1,2 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/mongrest').then(() => console.log('Database is Connected...')).catch((err) => console.error(err));
+mongoose.connect('mongodb://127.0.0.1:27017/task').then(() => console.log('Database is Connected...')).catch((err) => console.error(err));
+
+const User = require('./model/user');
+const Task = require('./model/task');
+
